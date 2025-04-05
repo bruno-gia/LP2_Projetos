f2ba2a9a9bd8e714af0097c33a41dd9995366ab4
bruno-gia <b_gia@hotmail.com>
2025-04-05 12:50:56 -0300

    learn(07-react): encerra a página 8 - Classe manipulação de borda parece não funcionar... - Retomar falando de componentes controlados e não controlados

---
+++ exibe-fotos/src/App.jsx
@@ -0,0 +1,17 @@
+import Busca from "./components/Busca"
+
+const App = () => {
+  return (
+  
+    <div className="grid border-1 border-200 justify-content-center w-9 m-auto">
+      <div className="col-12">
+        <h4>Exibir uma lista de...</h4>
+      </div>
+      <Busca/>
+    </div>
+
+
+  )
+}
+
+export default App
