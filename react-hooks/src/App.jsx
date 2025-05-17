dfaf075114e724799994bb18f15806bc0293d175
bruno-gia <b_gia@hotmail.com>
2025-05-17 12:52:45 -0300

    learn(08-react): encerra a seçao 2.10
    - retomar decidindo qual icone exibir para cada item
    - deicir qual item contrair e expandir

---
+++ react-hooks/src/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import Accordion from "./components/Accordion"
+
+const itens = [
+  {
+    titulo: "Java",
+    conteudo: "Linguagem compilada e interpretada"
+  },
+  {
+    titulo: "Python",
+    conteudo: "Linguagem interpretada e dinamicamente tipada"
+  },
+  {
+    titulo: "JavaScript",
+    conteudo: "Interpretada. Executa do lado do cliente e do lado do servidor"
+  }
+]
+const App = () => {
+  return (
+    <div>
+        <Accordion itens={itens}/>
+    </div>
+  )
+}
+
+export default App
