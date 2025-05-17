a928f0c6b68d202d0403d08b9364f31ae28c3193
bruno-gia <b_gia@hotmail.com>
2025-05-17 11:20:39 -0300

    learn(sem-apostila): faz back para pexels
    - nosso bak isola a chave da pexels
    - o front não conversa mais diretamente com a pexels

--- exibe-fotos/src/components/Busca.jsx
+++ exibe-fotos/src/components/Busca.jsx
@@ -1,27 +1,49 @@
-import React, { Component } from "react";
-import { Button } from "primereact/button";
-import { IconField } from "primereact/iconfield";
-import { InputIcon } from "primereact/inputicon";
-import { InputText } from "primereact/inputtext";
+// rcc
+import React, { Component } from 'react'
+import { Button } from 'primereact/button'
+import { IconField } from 'primereact/iconfield'
+import { InputIcon } from 'primereact/inputicon'
+import { InputText } from 'primereact/inputtext'
 
 export default class Busca extends Component {
-  state = {
-    termoDeBusca: "",
-  };
 
+  state = {
+    termoDeBusca: ''
+  }
+
+  onTermoAlterado = (event) => {
+    // console.log(event.target.value)
+    this.setState({termoDeBusca: event.target.value})
+  }
+  
+  onFormSubmit = (event) => {
+    event.preventDefault()
+    this.props.onBuscaRealizada(this.state.termoDeBusca)
+
+  }
   render() {
     return (
-      <div className="flex flex-column">
-        <IconField iconPosition="left">
-          <InputIcon className="pi pi-search" />
-          <InputText className="w-full" placeholder={this.props.dica} />
-        </IconField>
-        <Button className="mt-1" label="OK" outlined />
-      </div>
-    );
+      <form onSubmit={this.onFormSubmit}>
+        <div className='flex flex-column'>
+          <IconField            
+            iconPosition='left'>
+            <InputIcon  className='pi pi-search'/>
+            <InputText
+              onChange={this.onTermoAlterado}
+              value={this.state.termoDeBusca} 
+              className='w-full' 
+              placeholder={this.props.dica}/>  
+          </IconField>
+          <Button 
+            className='mt-1'
+            label='OK' 
+            outlined/>
+        </div>
+      </form>
+    )
   }
 }
 
 Busca.defaultProps = {
-  dica: "Buscar...",
-};
+  dica: 'Buscar...'
+}
--- exibe-fotos/src/main.jsx
+++ exibe-fotos/src/main.jsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import "primeicons/primeicons.css"
-import "primeflex/primeflex.min.css"
-import "primereact/resources/themes/md-dark-deeppurple/theme.css"
+import 'primeicons/primeicons.css'
+import 'primeflex/primeflex.min.css'
+import 'primereact/resources/themes/md-light-indigo/theme.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
